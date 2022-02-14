8dae49cefa847e79073b5db2e26bd51727754a93
meto1744 <meto17@abv.bg>
2022-02-14 09:04:36 +0200

    new project

---
+++ udrii 14.02.22/udrii 14.02.22/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace udrii_14._02._22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
