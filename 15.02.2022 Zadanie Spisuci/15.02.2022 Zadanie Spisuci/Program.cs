203d39119cf766d939fad8c05dda1f23e45fcb31
meto1744 <meto17@abv.bg>
2022-02-15 12:47:42 +0200

    Zadacha 15.02.22

---
+++ 15.02.2022 Zadanie Spisuci/15.02.2022 Zadanie Spisuci/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _15._02._2022_Zadanie_Spisuci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = Console.ReadLine().Split().ToList();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "print")
+                {
+                    Console.WriteLine(string.Join(" ", names));
+                }
+                string[] commands = input.Split();
+
+                switch (commands[0])
+                {
+                    case "add":
+                        {
+                            string elementToAdd = (commands[1]);
+                            names.Add(elementToAdd);
+                            if (names.Contains(elementToAdd))
+                            { 
+                                Console.WriteLine("Element already exists!");
+                            }
+                            break;
+                            
+                        }
+                    case "contains":
+                        {
+                            string nameToSearch = (commands[1]);
+                            if (names.Contains(nameToSearch))
+                            {
+                                Console.WriteLine(names.IndexOf(nameToSearch));
+                            }
+                            break;
+                        }
+                    case "insert":
+                        int numberToInsert = int.Parse(commands[1]);
+                        
+                        break;
+                    
+                       
+                }
+                    
+            }
+        }
+    }
+}
